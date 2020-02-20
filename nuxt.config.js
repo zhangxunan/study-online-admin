f0f835c298605a9a0fa4be6f83d66619e9e6275c
章旭楠 <zhang_xn1314@163.com>
2020-02-20 11:26:02 +0800

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,13 +64,24 @@ module.exports = {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
   /*
   ** Axios module configuration
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+    proxy: true
+  },
+  proxy: {
+    '/api': {
+      // target: 'http://example.com',
+      target: 'http://localhost:3000',
+      pathRewrite: {
+        '^/api': '/api'
+      }
+    }
   },
 
   /*
